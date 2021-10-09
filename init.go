d91a4675f46d7de06fb9e6f2ccb871c92abf50f2
cdle <798731886@qq.com>
2021-10-09 16:47:58 +0800

    update

--- init.go
+++ init.go
@@ -2,6 +2,7 @@ package wx
 
 import (
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/cdle/sillyGirl/core"
@@ -15,34 +16,73 @@ var api_url = wx.Get("api_url")
 func init() {
 	core.Server.POST("/yawx", func(c *gin.Context) {
 		data, _ := c.GetRawData()
-		s, _ := url.QueryUnescape(string(data))
+		s, err := url.QueryUnescape(string(data))
+		if err != nil {
+			return
+		}
+		args, err := url.ParseQuery(s)
+		if err != nil {
+			return
+		}
+		if args.Get("type") == "" {
+			return
+		}
+		// jmsg := &JsonMsg{}
+		// json.Unmarshal([]byte(args.Get("json_msg")), jmsg)
 		core.NotifyMasters(s)
+		core.Senders <- &Sender{}
 	})
 }
 
-type Message struct {
+type Sender struct {
+	leixing int
+	mtype   int
+	matches [][]string
+	deleted bool
+	goon    bool
+	value   url.Values
+}
+
+type JsonMsg struct {
+	Content         string `json:"content"`
+	FinalFromName   string `json:"final_from_name"`
+	FinalFromWxid   string `json:"final_from_wxid"`
+	FromName        string `json:"from_name"`
+	FromWxid        string `json:"from_wxid"`
+	MsgType         int    `json:"msg_type"`
+	Msgid           int    `json:"msgid"`
+	OriginalContent string `json:"original_content"`
+	SendOutType     int    `json:"send_out_type"`
+	Timestamp       int    `json:"timestamp"`
+	ToName          string `json:"to_name"`
+	ToWxid          string `json:"to_wxid"`
+}
+
+type PushMsg struct {
 	Type      int    `json:"type"`
 	Msg       string `json:"msg"`
 	FromWxid  string `json:"from_wxid"`
 	RobotWxid string `json:"robot_wxid"`
 }
 
-type Sender struct {
-	matches [][]string
-	deleted bool
-	goon    bool
-}
-
 func (sender *Sender) GetContent() string {
-	return ""
+	return sender.value.Get("msg")
 }
 
-func (sender *Sender) GetUserID() int {
-	return 0
+func (sender *Sender) GetUserID() interface{} {
+	if uid := sender.value.Get("final_from_wxid"); uid != "" {
+		return uid
+	} else {
+		return sender.value.Get("from_wxid")
+	}
 }
 
-func (sender *Sender) GetChatID() int {
-	return 0
+func (sender *Sender) GetChatID() interface{} {
+	if uid := sender.value.Get("final_from_wxid"); uid != "" {
+		return strings.Replace(sender.value.Get("from_wxid"), "@chatroom", "", -1)
+	} else {
+		return nil
+	}
 }
 
 func (sender *Sender) GetImType() string {
@@ -50,11 +90,15 @@ func (sender *Sender) GetImType() string {
 }
 
 func (sender *Sender) GetMessageID() int {
-	return 0
+	return core.Int(sender.value.Get("msgid"))
 }
 
 func (sender *Sender) GetUsername() string {
-	return ""
+	if uid := sender.value.Get("final_from_wxid"); uid != "" {
+		return sender.value.Get("final_from_name")
+	} else {
+		return sender.value.Get("from_name")
+	}
 }
 
 func (sender *Sender) IsReply() bool {
@@ -88,7 +132,6 @@ func (sender *Sender) GetAllMatch() [][]string {
 }
 
 func (sender *Sender) Get(index ...int) string {
-
 	i := 0
 	if len(index) != 0 {
 		i = index[0]
@@ -103,8 +146,7 @@ func (sender *Sender) Get(index ...int) string {
 }
 
 func (sender *Sender) IsAdmin() bool {
-
-	return true
+	return false
 }
 
 func (sender *Sender) IsMedia() bool {
